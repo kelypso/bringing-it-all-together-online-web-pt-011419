0bae3a0940667c21f858a46687257ec9d1c453b8
Kelsey Low <kklowlow@gmail.com>
2019-03-27 20:49:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed, :id 
   
   def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)} # accepts key value pairs
+    attributes.each {|key, value| self.send(("#{key}="), value)} # accepts key value pairs as args
     self.id ||= nil
   end
   
