65620eae2b1a4556c2fd089770ffeef241842d36
Kelsey Low <kklowlow@gmail.com>
2019-03-27 21:05:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,8 +65,15 @@ class Dog
   end
   
   def self.find_or_create_by(name:, breed:)
-    
-  end
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end 
   
   def self.find_by_name(name)
     sql = <<-SQL
